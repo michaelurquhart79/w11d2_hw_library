53af69f435461858a516fcf3dae13d025acb4629
Michael Urquhart <michaelurquhart@hotmail.com>
2019-07-23 17:18:31 +0100

    function to counBooks and addBook added and tested for Library class.

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> bookStock;
+
+    public Library() {
+        this.bookStock = new ArrayList<Book>();
+    }
+
+
+    public int countBooks() {
+        return this.bookStock.size();
+    }
+
+    public void addBook(Book book) {
+        this.bookStock.add(book);
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    private Book book1;
+    private Book book2;
+    private Library library;
+
+    @Before
+    public void before() {
+        book1 = new Book("Fight Club", "Geoff Palladnuik", "fiction");
+        book2 = new Book("The Life of Pi", "Some Guy", "fiction");
+        library = new Library();
+    }
+
+    @Test
+    public void libraryInitiallyHasNoBooks() {
+        assertEquals(0, library.countBooks());
+    }
+
+    @Test
+    public void canAddBookToLibrary() {
+        library.addBook(book1);
+        assertEquals(1, library.countBooks());
+    }
+
 }
